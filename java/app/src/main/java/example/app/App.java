5ce90d1c2c0b97a674cacbf7f754db93a11b7fd4
ianjevans <ian@cockroachlabs.com>
2022-03-09 15:23:22 -0800

    Check if JDBC_DATABASE_URL has been set and if not throw an error.

--- java/app/src/main/java/example/app/App.java
+++ java/app/src/main/java/example/app/App.java
@@ -3,15 +3,17 @@ package example.app;
 import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import java.util.Optional;
 import org.postgresql.ds.PGSimpleDataSource;
 
 public class App {
 
     public static void main(String[] args) {
-        
+
         try {
             PGSimpleDataSource ds = new PGSimpleDataSource();
-            ds.setUrl(System.getenv("JDBC_DATABASE_URL"));
+            ds.setUrl(Optional.ofNullable(System.getenv("JDBC_DATABASE_URL")).orElseThrow(
+  () -> new IllegalArgumentException("JDBC_DATABASE_URL is not set.")));
             Connection conn = ds.getConnection();
             Statement st = conn.createStatement();
             ResultSet rs = st.executeQuery("SELECT message FROM messages");
